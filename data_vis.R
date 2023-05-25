e54d61e34d291b107cef413acaf0644f9db04607
Data Science Institute <academic@datascienceinstitute.net>
2023-05-25 13:49:25 +0100

    Add files via upload

---
+++ data_vis.R
@@ -0,0 +1,97 @@
+## Data Visualisation
+## import the two data sets, telecom demo and telecom transaction
+demographic<-read.csv(file.choose())
+transaction<-read.csv(file.choose())
+head(demographic)
+head(transaction)
+tcalls<-aggregate(Calls~CustID, data=transaction, FUN=sum)
+head(tcalls)
+working<-merge(demographic, tcalls, by=("CustID"), all=TRUE)
+head(working)
+
+## create new column- age_group (18-30, 30-45, >45)
+working$age_group<-cut(working$Age, breaks=c(0,30,45,Inf), 
+                       labels=c("18-30","30-45",">45"))
+head(working)
+
+## Create a simple bar chart using ggplot2 library
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(working , aes ( x = age_group)) + geom_bar() 
+ggplot(working,aes(x=age_group,y=Calls))+
+  geom_bar(stat="identity",fill="green")+
+  labs(x="Age Groups",y="Total Calls",title="Bar Diagram")
+
+## reorder the bars
+ggplot(working,aes(reorder(age_group,Calls),Calls))+
+  geom_bar(stat="identity",fill="red")+
+  labs(x="Age Groups",y="Total Calls", title="Bar Diagram") 
+
+ggplot(working,aes(x=Gender,y=Calls))+
+  geom_bar(stat="identity",fill="maroon")+
+  labs(x= "Gender" ,y= "Total Calls",title="Bar Diagram")
+
+## Horizontal bar chart
+ggplot(working,aes(x=age_group,y=Calls))+
+  geom_bar(stat="identity",fill="orange")+
+  labs(x="Age Groups",y="Total Calls",title="Bar Diagram")+
+  coord_flip()
+
+## Stacked bar chart
+ggplot(working, aes(x=age_group))+
+  geom_bar(aes(fill=Gender))+ 
+  labs(x = "Age Group", y="No. of customers", title="Stacked bar chart")
+
+## Pie Chart
+pie_table<-table(working$age_group)
+pie_table
+pie_dataframe<-as.data.frame(pie_table)
+names(pie_dataframe)[1]<-"age_group"
+names(pie_dataframe)[2]<-"Count"
+pie_dataframe
+pct <- round(pie_dataframe$Count/sum(pie_dataframe$Count)*100)
+
+pie_dataframe$group_percent <- paste0(pie_dataframe$Age_group," ",pct,"%")
+pie(pie_dataframe$Count,labels = pie_dataframe$group_percent, 
+    col=rainbow(length(pie_dataframe$group_percent)),
+    main="Pie Chart of Age Groups")
+
+## Box and Whisker Plot
+boxplot(working$Calls, data= working, 
+        main="BOX PLOT (Total Calls)", ylab= "Total Calls", col= "cadetblue3")
+
+boxplot(Calls~age_group,data=working, 
+        main="BOX PLOT – (Total Calls – Age Group)",
+        xlab="Age Group",ylab="Total Calls", 
+        col=c("orange","green","cadetblue"))
+
+## Histogram
+hist(working$Calls, breaks=12, main = "HISTOGRAM – Total Calls", 
+     xlab = "Total Calls", ylab = "No. of Customers", col="darkorange")
+## Density Plot
+working_den<-density(working$Calls) 
+plot(working_den, main="DENSITY PLOT - Calls",xlab="Calls")
+polygon(working_den, col="yellowgreen")
+
+## Stem and Leaf chart
+stem(working$Calls)
+
+## Pareto chart
+
+install.packages("RColorBrewer")
+library(RColorBrewer)
+##col=brewer.pal(n,"palette")
+install.packages("qcc")
+library(qcc)
+
+telecom1<-aggregate(Calls~age_group,data = working, FUN=sum)
+
+pareto.chart(telecom1$Calls, xlab= "Age Groups" ,ylab= "Total Calls" ,           
+             main = "PARETO CHART : Age Group",col=brewer.pal(3,"Blues"), 
+             names.arg=telecom1$age_group)
+
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(working, aes(x=Calls))+ geom_histogram(binwidth=100, fill="darkorange")+
+  labs(x="Total Calls", y="No. of customers", title="HISTOGRAM")
+
---
+++ r_packages.R
@@ -0,0 +1,125 @@
+## dplyr package
+install.packages("dplyr")
+library(dplyr)
+data<-read.csv(file.choose())
+
+## creating subset using filter and select
+data1<-filter(data,Grade=="GR1",Location=="MUMBAI")
+head(data1)
+data2<-select(data,First_Name, Location, ba)
+head(data2)
+data3<-select(data,-(Last_Name:Location))
+head(data3)
+
+## arrange
+a1<-arrange(data,desc(ba))
+head(a1)
+a2<-arrange(data,Grade,Location,ba)
+head(a2)
+
+## mutate, creating new column
+m1<-mutate(data, total=ba+ms)
+head(m1)
+m2<-mutate(data, total=ba-ms)
+head(m2)
+
+## summarise and group_by
+summarise(data,meanba=mean(ba,na.rm=TRUE),
+          medianba=median(ba,na.rm=TRUE))
+loc_wise<-group_by(data,Location)
+summarise(loc_wise,count=n(),mean=mean(ba,na.rm=TRUE))
+
+## pipe operator
+data %>% 
+  select(First_Name,Grade,Location) %>% 
+  filter(Location=="MUMBAI")
+
+sample_n(data,7)
+sample_frac(data,0.2)
+
+## Rank
+data %>%
+  mutate(Rank=dense_rank(desc(ba)))%>%
+  arrange(Rank) %>%
+  head()
+
+## package data.table
+install.packages("data.table")
+library(data.table)
+dt1<- data.table(ID=1:7000000,
+                 Capacity=sample(100:1000,size=50,replace=F),
+                 Code=sample(LETTERS[1:4],50,replace=T),
+                 State=rep(c("Alabama","Indiana","Texas","Nevada")))
+head(dt1, 4)
+## creating subset using row indexing
+subset1<-dt1[3:11,]
+head(subset1)
+subset2<-dt1[Code=="B" & State=="Texas"]
+head(subset2)
+
+## creating subset using columns
+sub_columns<-dt1[,.(ID,Capacity)]
+head(sub_columns, 3)
+
+setkey(dt1,Code,State)
+sub_key<-dt1[.("C","Alabama")]
+head(sub_key, 2)
+
+sub_key2<-dt1[.("C")]
+head(sub_key2)
+sub_key3 <- dt1[.(unique(Code),"Alabama")]
+head(sub_key3)
+
+## sorting
+dt_order1<-dt1[order(Code,-State)]
+head(dt_order1)
+dt_order2<-dt1[order(-Code,-Capacity)]
+head(dt_order2)
+
+## Modifying data, add a column
+dt1[,new_capacity:=Capacity+5]
+head(dt1)
+dt1[State=="Alabama",State:="Al"]
+head(dt1)
+## delete a column
+dt1[,c("Capacity"):=NULL]
+head(dt1,2)
+## chaining of commands
+dt1[,new_capacity:=Capacity + 5] [State=="Alabama",State:="Al"] [,"Capacity":=NULL]
+head(dt1)
+## renaming
+setnames(dt1,old="new_capacity" , new = "New_Capacity")
+head(dt1,2)
+setkey(dt1, New_Capacity,State)
+
+## aggregate function
+DT_agg <- dt1[,sum(New_Capacity),by=State]
+DT_agg
+DT_agg <- dt1[,.(Totalcapacity=sum(New_Capacity)),by=State]
+DT_agg
+
+## Package tidyr
+install.packages("tidyr")
+library(tidyr)
+stud_data<-read.csv(file.choose())
+
+# gather(stud_data), convert wide format to long format
+longformat<-gather(stud_data,Subjects,Marks,Maths,Economics,Statistics)
+longformat
+spread(longformat,Subjects,Marks,fill=0)
+
+## separate function
+empid<-c(101,102,103,104)
+location<-c("Mumbai","Delhi","Delhi","Mumbai")
+address<-c("4/Churchgate","12/Rohini","8/Pitampura", "21/Andheri")
+date<-c("2016-10-09","2010-11-01","2009-09-23","1990-02-30")
+empdata<-data.frame(empid,location,address,date)
+
+empdata
+sep_date<-separate(empdata,date,into=c("Year","Month","Date"))
+sep_date
+sep_address<-separate(empdata,address,into=c("sector","area"),sep="/", convert=TRUE)
+sep_address
+unite_date<-unite(sep_date,date,c(Year,Month,Date),sep="/")
+unite_date
+
